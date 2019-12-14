750d825f52ef3c3d25f98b683d9d7c41992d2c46
paullee714 <ba93love@gmail.com>
2019-12-14 18:05:46 +0900

    Movie 리스트를 TimeOut, state를 사용해서 불러오기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -12,8 +12,13 @@ class App extends Component{
 
 //state가 변경되면 다시한번 render
   state = {
-    greeting : 'hello!',
-    movies:[
+
+  }
+
+  componentDidMount(){
+    setTimeout(()=> {
+      this.setState({
+        movies:[
       {
         title : "Matrix",
         poster : "https://633987.smushcdn.com/907311/wp-content/uploads/2019/08/lede-the-matrix-1300x731.jpg?lossy=1&strip=1&webp=1"
@@ -29,32 +34,27 @@ class App extends Component{
       {
         title :"starwars",
         poster:"https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fstar-wars-jedi-fallen-order%2Fhome%2FSWFJO_Standard_Keyart_1920x1080-1920x1080-7a42241ea451299ac73036ac61eb73c1184753e0.jpg"
+      },
+      {
+          title:"Transporting",
+          poster:"http://collider.com/wp-content/uploads/trainspotting_movie_image_ewan_mcgregor_01.jpg"
       }
     ]
-  }
-
-  componentDidMount(){
-    setTimeout(()=> {
-      this.setState({
-        movies: [
-          {
-            title:"Transporting",
-            poster:"http://collider.com/wp-content/uploads/trainspotting_movie_image_ewan_mcgregor_01.jpg"
-          },
-          ...this.state.movies //이전영화를 한번 더 부르는 것
-        ]
       })
     },2000)
   }
 
-
+  _renderMovies = () =>{
+    const movies = this.state.movies.map((movie, index) => {
+          return <Movie title={movie.title} poster = {movie.poster} key={index}/>
+    })
+    return movies
+  }
 
   render(){
     return(
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster = {movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading...'}
       </div>
     );
   }
