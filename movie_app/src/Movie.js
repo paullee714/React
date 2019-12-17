15d77fe49ada0064e90069257625303a9033dce3
paullee714 <ba93love@gmail.com>
2019-12-17 21:30:27 +0900

    set More Infomation about Movie, set in View

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -4,31 +4,57 @@ import './Movie.css';
 
 
 //state가 없느 function (클래스대신에 사용할 수 있음)
-function Movie({title,poster}){
+function Movie({title,poster, genres, synopsis}){
     return(
-    <div>
-        <MoviePoster poster={poster}/>
-        <h1>{title} </h1>
+    <div className="Movie">
+        <div className="Movie__Columns">
+            <MoviePoster poster={poster} title={title} alt={title}/>
+        </div>
+        <div className="Movie__Columns">
+            <h1>{title} </h1>
+            <div className="Movie__Genres">
+                {genres.map((genre,index) => <MovieGenre genre={genre} key={index}/>)}
+            </div>
+            <p className="Movie__Synopsis">
+                {synopsis}
+            </p>
+        </div>
+        
     </div>
     )
 }
 
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
     return (
-        <img src={poster} alt="Movie Poster"/>
+        <img src={poster} title = {alt} alt={alt}/>
     )
 }
+
+function MovieGenre({genre}){
+    return(
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+
 // function의 propType를 체크하는 법
 
 MoviePoster.propTypes = {
-    poster : PropTypes.string.isRequired
+    title : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired,
+    alt : PropTypes.string.isRequired
 }
 
 Movie.propTypes = {
+    poster : PropTypes.string.isRequired,
     title : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
+    genres :PropTypes.array.isRequired,
+    synopsis : PropTypes.string.isRequired
 }
 
+MovieGenre.propTypes= {
+    genre: PropTypes.string.isRequired
+}
 
 
 export default Movie;
